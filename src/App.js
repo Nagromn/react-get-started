9fa9032a94a304740841118f14f67f61035a2c48
Nagromn <morganwtt-dev@pm.me>
2024-04-08 17:16:09 +0200

    correction composant Dish

--- src/App.js
+++ src/App.js
@@ -6,7 +6,24 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <Dish />
+            <Dish
+                image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+                alt="Tacos à l’unité"
+                name="Tacos à l’unité"
+                price="3€"
+            />
+            <Dish
+                image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+                alt="Enchiladas"
+                name="Enchiladas"
+                price="12€"
+            />
+            <Dish
+                image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+                alt="Mole poblano"
+                name="Mole poblano"
+                price="15€"
+            />
             <Footer />
         </div>
     );
--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,32 +1,11 @@
 import "../assets/styles/dish.css";
 
-const Dish = () => {
+const Dish = (props) => {
     return (
         <div>
-            <div>
-                <img
-                    src="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                    alt="Tacos à l’unité"
-                />
-                <h2>Tacos à l’unité</h2>
-                <strong>Prix : 3€</strong>
-            </div>
-            <div>
-                <img
-                    src="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                    alt="Enchiladas"
-                />
-                <h2>Enchiladas</h2>
-                <strong>Prix : 12€</strong>
-            </div>
-            <div>
-                <img
-                    src="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                    alt="Mole poblano"
-                />
-                <h2>Mole poblano</h2>
-                <strong>Prix : 15€</strong>
-            </div>
+            <img src={props.image} alt={props.alt} />
+            <h2>{props.name}</h2>
+            <strong>Prix : {props.price}</strong>
         </div>
     );
 };
