86945ab457625d96b7475b365d55e2b8f7e60d3a
Nagromn <morganwtt-dev@pm.me>
2024-04-10 11:41:25 +0200

    add dish mapping

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,38 +4,49 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const Home = () => {
+    const dishes = [
+        {
+            id: 1,
+            image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+            alt: "Tacos à l’unité",
+            name: "Tacos à l’unité",
+            price: 3,
+            slug: "tacos-a-l-unite",
+        },
+        {
+            id: 2,
+            image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+            alt: "Enchiladas",
+            name: "Enchiladas",
+            price: 12,
+            slug: "enchiladas",
+        },
+        {
+            id: 3,
+            image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+            alt: "Mole poblano",
+            name: "Mole poblano",
+            price: 15,
+            slug: "mole-poblano",
+        },
+    ];
+
     return (
         <>
             <Container>
                 <Row>
-                    <Col>
-                        <Dish
-                            image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                            alt="Tacos à l’unité"
-                            name="Tacos à l’unité"
-                            price="3€"
-                            slug="tacos-a-l-unite"
-                        />
-                    </Col>
-                    <Col>
-                        <Dish
-                            image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                            alt="Enchiladas"
-                            name="Enchiladas"
-                            price="12€"
-                            slug="enchiladas"
-                        />
-                    </Col>
-
-                    <Col>
-                        <Dish
-                            image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                            alt="Mole poblano"
-                            name="Mole poblano"
-                            price="15€"
-                            slug="mole-poblano"
-                        />
-                    </Col>
+                    {dishes.map((dish) => (
+                        <Col>
+                            <Dish
+                                key={dish.id}
+                                image={dish.image}
+                                alt={dish.alt}
+                                name={dish.name}
+                                price={`${dish.price}€`}
+                                slug={dish.slug}
+                            />
+                        </Col>
+                    ))}
                 </Row>
             </Container>
         </>
