07902f11f5bf235c2bf1c74d58dc2d3387b0a6ee
Nagromn <morganwtt-dev@pm.me>
2024-04-10 17:00:16 +0200

    add dishes useEffect and data json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,47 +3,24 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import dishesData from "../data/dishes.json";
 
 const Home = () => {
-    const dishes = [
-        {
-            id: 1,
-            image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
-            alt: "Tacos à l’unité",
-            name: "Tacos à l’unité",
-            price: 3,
-            slug: "tacos-a-l-unite",
-            new: true,
-        },
-        {
-            id: 2,
-            image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
-            alt: "Enchiladas",
-            name: "Enchiladas",
-            price: 12,
-            slug: "enchiladas",
-            new: false,
-        },
-        {
-            id: 3,
-            image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
-            alt: "Mole poblano",
-            name: "Mole poblano",
-            price: 15,
-            slug: "mole-poblano",
-            new: false,
-        },
-    ];
-
     const [showNewOnly, setShowNewOnly] = useState(false);
+    const [dishes, setDishesData] = useState([]);
+
+    useEffect(() => {
+        const filteredDishes = () => {
+            return showNewOnly
+                ? dishesData.filter((dish) => dish.new)
+                : dishesData;
+        };
+        setDishesData(filteredDishes);
+    }, [showNewOnly]);
 
     const handleShowNewOnly = () => {
         setShowNewOnly(!showNewOnly);
-    };
-
-    const filteredDishes = () => {
-        return showNewOnly ? dishes.filter((dish) => dish.new) : dishes;
     };
 
     return (
@@ -52,11 +29,12 @@ const Home = () => {
                 {!showNewOnly ? "Nouveautés uniquement" : "Voir tous les plats"}
             </Button>
             <Row>
-                {filteredDishes().map((dish) => (
-                    <Col key={dish.id}>
-                        <Dish {...dish} />
-                    </Col>
-                ))}
+                {dishes.length > 0 &&
+                    dishes.map((dish) => (
+                        <Col key={dish.id}>
+                            <Dish {...dish} />
+                        </Col>
+                    ))}
             </Row>
         </Container>
     );
